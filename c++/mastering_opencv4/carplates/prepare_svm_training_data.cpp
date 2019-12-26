3b92199c1cb8abf727a9b8196bd847481fafb661
Vincent <vincentcheng929@gmail.com>
2019-12-26 14:10:52 +0800

    fix prepare svm training data

--- c++/mastering_opencv4/carplates/prepare_svm_training_data.cpp
+++ c++/mastering_opencv4/carplates/prepare_svm_training_data.cpp
@@ -18,9 +18,9 @@ int main (int argc, char **argv) {
         plate_path = argv[3];
         notplate_path = argv[4];
     } else {
-        std::cout << "Usage: \n << argv[0] << 
+        std::cout << "Usage: \n" << argv[0] << 
         " <num plates > <num non plates> <path to plate files> " <<
-        " <path to non plate files" << std::endl;
+        " <path to not plate files>" << std::endl;
     }
 
     cv::Mat classes;
@@ -32,27 +32,33 @@ int main (int argc, char **argv) {
         std::stringstream ss(std::stringstream::in | std::stringstream::out);
         ss << plate_path << i << ".jpg";
         cv::Mat img = cv::imread(ss.str(), 0);
-        im = img.reshape(1, 1);
-        training_imgs.push_back(img);
+        if (img.empty()) {
+            std::cout << "Failed to read image from" << ss.str() << std::endl;
+        }
+        img = img.reshape(1, 1);
+        training_images.push_back(img);
         training_labels.push_back(1);
     }
 
-    for (int i = 0; i < num_noplates; i++) {
+    for (int i = 0; i < num_notplates; i++) {
         std::stringstream ss(std::stringstream::in | std::stringstream::out);
-        ss << noplate_path << i << ".jpg";
-        cv::Mat img = imread(ss.str(), 0);
+        ss << notplate_path << i << ".jpg";
+        cv::Mat img = cv::imread(ss.str(), 0);
+        if (img.empty()) {
+            std::cout << "Failed to read image from" << ss.str() << std::endl;
+        }
         img = img.reshape(1, 1);
-        training_imgs.push_back(img);
+        training_images.push_back(img);
         training_labels.push_back(0);
     }
 
     cv::Mat(training_images).copyTo(training_data); 
     training_data.convertTo(training_data, CV_32FC1);
-    cv::Mat(traning_labels).copyTo(classes);
+    cv::Mat(training_labels).copyTo(classes);
 
     cv::FileStorage fs("svm.xml", cv::FileStorage::WRITE);
     fs << "training_data" << training_data;
-    fs << "classes" << classes;
+    fs << "training_labels" << classes;
     fs.release();
 
     return 0;
